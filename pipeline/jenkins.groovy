408e01b2581b184f3aef05ff5f5137b914eedce0
Oleksandr Shvaitser <al.schweizer@gmail.com>
2023-06-19 23:27:56 +0300

    Modified jenkins.groovy

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -7,13 +7,32 @@ pipeline {
 
     }
     stages {
-        stage('Example') {
+        stage('Build') {
             steps {
                 echo "Build for platform ${params.OS}"
-
                 echo "Build for arch: ${params.ARCH}"
+                echo "BILD EXECUTION STARTED"
+                sh "make build"
 
             }
         }
+        
+        stage('Image') {
+            steps {
+                echo "BILD EXECUTION STARTED"
+                sh "make image"
+
+            }
+        }
+
+        stage('Push') {
+            steps {
+                script {
+                    docker.withRegistry( "","dockerhub" ) {
+                    sh "make image"
+                    }
+                }
+            }
+        }    
     }
 }
